3d15c99db3e3b8ae2f9464f02a11b82f3c48d2e4
guillebags <guillebags@gmail.com>
2024-05-19 18:30:01 +0200

    setup messages component

---
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css',
+})
+export class MessagesComponent {}
