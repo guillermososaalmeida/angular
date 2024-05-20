4282d6be1814cc7d2017c7c5674bb59d48f96cfa
guillebags <guillebags@gmail.com>
2024-05-20 19:03:44 +0200

    router without navigation

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppModule } from './app.module';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppModule],
+  imports: [AppModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [FormsModule, CommonModule, AppRoutingModule],
+  imports: [FormsModule, CommonModule],
   exports: [HeroesComponent, MessagesComponent],
   providers: [],
 })
