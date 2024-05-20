b4363b0b28c9c8a0c85657565ef665680b88d79b
guillebags <guillebags@gmail.com>
2024-05-20 19:38:49 +0200

    navigation to heroes / dashboard set up

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent],
   imports: [FormsModule, CommonModule],
-  exports: [HeroesComponent, MessagesComponent],
+  exports: [HeroesComponent, MessagesComponent, RouterModule],
   providers: [],
 })
 export class AppModule {}
